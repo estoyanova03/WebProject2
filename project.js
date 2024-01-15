d820857a236a8cb1812d947bc65d1418383f0e91
Emiliya-Stoyanova <esstoyanova03@gmail.com>
2024-01-15 16:49:26 +0200

    *

--- project.js
+++ project.js
@@ -2,26 +2,3 @@ function expand() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-
-/**document.addEventListener('DOMContentLoaded', function() {
-  const menuBtn = document.getElementsByClassName('menuButton');
-  const dropdown = document.getElementsByClassName('dropdown');
-  menuBtn.addEventListener('click', () => {
-    dropdown.('.open');
-  });
-}); */
-
-// function expand() {
-//   document.getElementById("menuButton").classList.toggle("dropdown");
-// }
-
-
-
-
-/*document.addEventListener('DOMCOntentLoaded', () => {
-  function expand() {
-    document.querySelector('menuButton').addEventListener('click', () => {
-      document.querySelector('dropdown').classList.add('open');
-    })
-  };
-}); */
