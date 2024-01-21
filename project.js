21ab3895ae32214c37d8f211e0b116ccc979cf95
Emiliya-Stoyanova <esstoyanova03@gmail.com>
2024-01-22 00:10:28 +0200

    new

--- project.js
+++ project.js
@@ -2,3 +2,15 @@ function expand() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
+window.onclick = function(event) {
+  if (!event.target.matches('.menuButton')) {
+    var dropdowns = document.getElementsByClassName("dropInfo");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
